12bbbde384c3069e78dd213e1153352440c55e61
fxpw <fxpw@outlook.com>
2023-11-28 13:35:55 +0300

    new lesson

--- lesson1/main.py
+++ lesson1/main.py
@@ -4,39 +4,39 @@ from flask import Flask
 
 
 def get_valutes_list():
-    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
-    response = requests.get(url)
-    data = json.loads(response.text)
-    valutes = list(data['Valute'].values())
-    return valutes
+	url = "https://www.cbr-xml-daily.ru/daily_json.js"
+	response = requests.get(url)
+	data = json.loads(response.text)
+	valutes = list(data["Valute"].values())
+	return valutes
 
 
 app = Flask(__name__)
 
 
 def create_html(valutes):
-    text = '<h1>Курс валют</h1>'
-    text += '<table>'
-    text += '<tr>'
-    for _ in valutes[0]:
-        text += f'<th><th>'
-    text += '</tr>'
-    for valute in valutes:
-        text += '<tr>'
-        for v in valute.values():
-            text += f'<td>{v}</td>'
-        text += '</tr>'
+	text = "<h1>Курс валют</h1>"
+	text += "<table>"
+	text += "<tr>"
+	for _ in valutes[0]:
+		text += "<th><th>"
+	text += "</tr>"
+	for valute in valutes:
+		text += "<tr>"
+		for v in valute.values():
+			text += f"<td>{v}</td>"
+		text += "</tr>"
 
-    text += '</table>'
-    return text
+	text += "</table>"
+	return text
 
 
 @app.route("/")
 def index():
-    valutes = get_valutes_list()
-    html = create_html(valutes)
-    return html
+	valutes = get_valutes_list()
+	html = create_html(valutes)
+	return html
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
---
+++ lesson3/2seq,py
@@ -0,0 +1,5 @@
+m_list = []
+str_for_parse = str(input("vvedite nums whit / separator "))
+m_list = list(str_for_parse.split("/"))
+print(",".join(m_list))
+	
